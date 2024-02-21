3744509b8cd83ed84078b17e893a509eb5da93e3
SeekingSm <pavan5dcp@gmail.com>
2024-02-21 07:59:38 +0530

    Uploading the local testingTutorial code to github

---
+++ src/test/Day1.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.testng.annotations.Test;
+
+
+public class Day1 {
+
+	@Test 
+	public void testm1() {
+	System.out.println("Day1-testm1");
+	
+	}
+	@Test 
+	public void testm2() {
+	System.out.println("Day1-testm2");
+	
+	}
+	@Test 
+	public void testm3() {
+	System.out.println("Day1-testm3");
+	
+	}
+	
+	@Test 
+	public void testm4() {
+	System.out.println("Day1-testm4");
+	
+	}
+	@Test 
+	public void testm5() {
+	System.out.println("Day1-testm5");
+	
+	}
+	
+	@Test 
+	public void testm6() {
+	System.out.println("Day1-testm6");
+	
+	}
+}
---
+++ src/test/Day2.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.testng.annotations.Test;
+
+
+public class Day2 {
+
+	@Test 
+	public void testm1() {
+	System.out.println("Day2-testm1");
+	
+	}
+	@Test 
+	public void testm2() {
+	System.out.println("Day2-testm2");
+	
+	}
+	@Test 
+	public void testm3() {
+	System.out.println("Day2-testm3");
+	
+	}
+	
+	@Test 
+	public void testm4() {
+	System.out.println("Day2-testm4");
+	
+	}
+	@Test 
+	public void testm5() {
+	System.out.println("Day2-testm5");
+	
+	}
+	
+	@Test 
+	public void testm6() {
+	System.out.println("Day2-testm6");
+	
+	}
+}
